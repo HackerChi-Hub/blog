48142c66bbc8c26a83860e7167cef8257419d486
HackerChi <hackerchi99@gmail.com>
2025-11-25 12:35:18 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,13 @@ const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'blog';
 
 const nextConfig = {
+  // 关键：启用静态导出模式（取代 next export 命令）
+  output: 'export',
+
+  // 给 GitHub Pages 的子路径用
   basePath: isProd ? `/${repoName}` : '',
   assetPrefix: isProd ? `/${repoName}/` : '',
+
   trailingSlash: true,
   reactStrictMode: true
 };
