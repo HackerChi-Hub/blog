45bf6c28450c5478f24d7aa2c8b12fac2cc02ba8
HackerChi <hackerchi99@gmail.com>
2025-11-27 19:05:44 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,47 @@
-import '@/styles/globals.css'; // 如果已有全局样式，请保持
+// pages/_app.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
+// 若有全局样式，按需导入：import '@/styles/globals.css';
 
-const GA_MEASUREMENT_ID = 'G-PLLG23LT3H'; // 替换为你的 Measurement ID
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-XXXXXXXXXX';
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!GA_MEASUREMENT_ID) return;
+
+    const handleRouteChange = (url) => {
+      window.gtag?.('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="ga-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+      {GA_MEASUREMENT_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />
     </>
   );
