c7871c729a255f76394105fd32df89bc53c731d9
HackerChi <hackerchi99@gmail.com>
2025-12-06 09:08:32 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,16 @@
-/** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
+// next.config.mjs
 const repoName = 'blog';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // 关键：启用静态导出模式（取代 next export 命令）
   output: 'export',
-
-  // 如果你以后只通过 hac.top 访问，而不再走 hackerchi-hub.github.io/blog
-  // 可以直接不用 basePath / assetPrefix
-  // basePath: '',
-  // basePath: '',
-  // assetPrefix: '',
-
   trailingSlash: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : ''
 };
 
 export default nextConfig;
