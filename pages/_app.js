b175465fc422a009ca4180461fe279f581462a14
HackerChi <hackerchi99@gmail.com>
2026-01-22 10:56:43 +0800

    优化 Notice 公告栏：添加图片支持、提取图片注释、调整样式和布局

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import Script from 'next/script';
 import '../styles/globals.css';
 import 'react-notion-x/src/styles.css';
@@ -125,6 +126,15 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        {/* Favicon 和图标设置 */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       {GA_MEASUREMENT_ID && (
         <>
           <Script
